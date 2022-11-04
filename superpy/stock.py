a8dbced0865a6f2d13351f549ab337159cbdb982
lindavos-dot <lindavos123@hotmail.com>
2022-11-04 10:12:59 +0100

    20221104

---
+++ superpy/stock.py
@@ -0,0 +1,2 @@
+# voorraad document aanmaken, van waaruit ook de sold en over datum artikelen vanuit worden gehaald (?)
+# indeling: id, category, naam product, aantal op voorraad, houdbaarheidsdatum, 
