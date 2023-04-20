824c916885fc12cf405e36af37c53e71af0afe0c
Linda Vos <lindavos123@hotmail.com>
2023-04-20 14:18:08 +0200

    20230420

--- superpy/test.py
+++ superpy/test.py
@@ -6,6 +6,28 @@ from rich.console import Console
 import pandas as pd
 from csv2pdf import convert
 
+def drop_line(product, expiration_date):
+    path = get_path('current_stock.csv')
+    with open(path, mode= 'r') as file:
+        csv_reader = csv.DictReader(file)
+        listing = list(csv_reader)
+
+        for row in listing:
+            if row['product'] == product and row['expiration_date'] == expiration_date:
+                listing.remove(row)
+
+    header = ['id','mutation_date','product','amount','price','expiration_date']       
+    with open(get_path('current_stock.csv'), mode= 'w', newline= '') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=header)
+        writer.writeheader() 
+        writer.writerows(listing) 
+        
+
+
+
+
+#drop_line('koek', '2023-10-23')
+
 
 
 
@@ -23,9 +45,6 @@ def buy(product, amount, price, expiration_date):
 
     with open(path, mode= 'r') as file:
         csv_reader = csv.DictReader(file)
-        
-        old_row = {}
-        new_row = {}
 
         for row in csv_reader:
             #print(row['product'])
@@ -49,9 +68,12 @@ def buy(product, amount, price, expiration_date):
             if row is None:
                 #print("d")
                 append_new_lines(path, id, product, amount, price, expiration_date)                
+             
 
 
-buy('Kaas', 1, 2, '2023-10-17')
+#buy('Koek', 1, 2, '2023-10-24')
+#buy('Koek', 1, 2, '2023-10-23')
+#buy('Koek', 1, 2, '2023-10-22')
 
 
 
@@ -71,7 +93,27 @@ with open(path, mode= 'r') as file:
     csv_reader = csv.DictReader(file)
 
     for row in csv_reader:
-        print(row['product'])                
+        print(row['product'])    
+
+
+                old_row = {}
+        new_row = {}
+
+        for row in csv_reader:
+            #print(row['product'])
+            if row['product'] == product and row['expiration_date'] == expiration_date:
+                               
+                old_row = row
+                new_row = row
+                print(path, old_row['product'])
+                deleteline(path, old_row['product'])
+                
+                current_amount = new_row['amount']               
+                new_amount = int(current_amount) + int(amount)
+                new_row['amount'] = new_amount
+                
+                print(path, new_row['id'], new_row['product'], new_row['amount'], new_row['price'], new_row['expiration_date'])
+                append_new_lines(path, new_row['id'], new_row['product'], new_row['amount'], new_row['price'], new_row['expiration_date'])           
 '''
         
 
